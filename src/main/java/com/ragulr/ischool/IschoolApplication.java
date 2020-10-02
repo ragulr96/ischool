1beb74731b745d301b88b39a79db8c7e629d9515
kajendran97 <chandreswaran.2016323@iit.ac.lk>
2020-10-02 21:06:56 +0530

    add comments to main class

--- src/main/java/com/ragulr/ischool/IschoolApplication.java
+++ src/main/java/com/ragulr/ischool/IschoolApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IschoolApplication {
-
+	#main methods start here ischool
 	public static void main(String[] args) {
 		SpringApplication.run(IschoolApplication.class, args);
 	}
